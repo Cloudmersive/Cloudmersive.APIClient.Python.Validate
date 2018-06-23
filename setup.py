89c465ba9a5dd3607c812a54b889f658c7bfc5fd
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-06-23 14:45:17 -0700

    Updated

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 
 from setuptools import setup, find_packages  # noqa: H301
 
-NAME = "cloudmersive-validate-api-client"
+NAME = "cloudmersive.validate.api.client"
 VERSION = "1.0.0"
 # To install the library, run the following
 #
