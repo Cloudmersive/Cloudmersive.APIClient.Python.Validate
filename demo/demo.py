61ece7580f7b7eb09345a8ecc80101665e384daa
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-06-23 14:48:49 -0700

    Added

---
+++ demo/demo.py
@@ -0,0 +1,20 @@
+﻿from __future__ import print_function
+import time
+import cloudmersive_validate_api_client
+from cloudmersive_validate_api_client.rest import ApiException
+from pprint import pprint
+
+# Configure API key authorization: Apikey
+cloudmersive_validate_api_client.configuration.api_key['Apikey'] = 'YOUR_API_KEY'
+# Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
+# cloudmersive_validate_api_client.configuration.api_key_prefix['Apikey'] = 'Bearer'
+# create an instance of the API class
+api_instance = cloudmersive_validate_api_client.DomainApi()
+domain = 'domain_example' # str | Domain name to check, for example \"cloudmersive.com\".  The input is a string so be sure to enclose it in double-quotes.
+
+try:
+    # Validate a domain name
+    api_response = api_instance.domain_check(domain)
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling DomainApi->domain_check: %s\n" % e)
