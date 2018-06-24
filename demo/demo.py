a52a2e4225cbdc5d0f8e011801289731dadc4f80
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-06-23 17:11:32 -0700

    Added

--- demo/demo.py
+++ demo/demo.py
@@ -5,7 +5,7 @@ from cloudmersive_validate_api_client.rest import ApiException
 from pprint import pprint
 
 # Configure API key authorization: Apikey
-cloudmersive_validate_api_client.configuration.api_key['Apikey'] = 'YOUR_API_KEY'
+cloudmersive_validate_api_client.configuration.api_key['Apikey'] = 'f0c513bc-8c00-4491-830e-3e83b015feb6'
 # Uncomment below to setup prefix (e.g. Bearer) for API key, if needed
 # cloudmersive_validate_api_client.configuration.api_key_prefix['Apikey'] = 'Bearer'
 # create an instance of the API class
