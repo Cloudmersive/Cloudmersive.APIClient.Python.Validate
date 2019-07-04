0fe600d766b8548bee8adfb06ac526426279c32d
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-07-04 16:56:37 -0700

    Added 2.0.2

--- cloudmersive_validate_api_client/models/full_email_validation_response.py
+++ cloudmersive_validate_api_client/models/full_email_validation_response.py
@@ -37,7 +37,9 @@ class FullEmailValidationResponse(object):
         'valid_domain': 'bool',
         'valid_smtp': 'bool',
         'is_catchall_domain': 'bool',
-        'domain': 'str'
+        'domain': 'str',
+        'is_free_email_provider': 'bool',
+        'is_disposable': 'bool'
     }
 
     attribute_map = {
@@ -47,10 +49,12 @@ class FullEmailValidationResponse(object):
         'valid_domain': 'Valid_Domain',
         'valid_smtp': 'Valid_SMTP',
         'is_catchall_domain': 'IsCatchallDomain',
-        'domain': 'Domain'
+        'domain': 'Domain',
+        'is_free_email_provider': 'IsFreeEmailProvider',
+        'is_disposable': 'IsDisposable'
     }
 
-    def __init__(self, valid_address=None, mail_server_used_for_validation=None, valid_syntax=None, valid_domain=None, valid_smtp=None, is_catchall_domain=None, domain=None):  # noqa: E501
+    def __init__(self, valid_address=None, mail_server_used_for_validation=None, valid_syntax=None, valid_domain=None, valid_smtp=None, is_catchall_domain=None, domain=None, is_free_email_provider=None, is_disposable=None):  # noqa: E501
         """FullEmailValidationResponse - a model defined in Swagger"""  # noqa: E501
 
         self._valid_address = None
@@ -60,6 +64,8 @@ class FullEmailValidationResponse(object):
         self._valid_smtp = None
         self._is_catchall_domain = None
         self._domain = None
+        self._is_free_email_provider = None
+        self._is_disposable = None
         self.discriminator = None
 
         if valid_address is not None:
@@ -76,6 +82,10 @@ class FullEmailValidationResponse(object):
             self.is_catchall_domain = is_catchall_domain
         if domain is not None:
             self.domain = domain
+        if is_free_email_provider is not None:
+            self.is_free_email_provider = is_free_email_provider
+        if is_disposable is not None:
+            self.is_disposable = is_disposable
 
     @property
     def valid_address(self):
@@ -237,6 +247,52 @@ class FullEmailValidationResponse(object):
         """
 
         self._domain = domain
+
+    @property
+    def is_free_email_provider(self):
+        """Gets the is_free_email_provider of this FullEmailValidationResponse.  # noqa: E501
+
+        True if the email domain name is a free provider (typically a free to sign up web email provider for consumers / personal use), false otherwise.  # noqa: E501
+
+        :return: The is_free_email_provider of this FullEmailValidationResponse.  # noqa: E501
+        :rtype: bool
+        """
+        return self._is_free_email_provider
+
+    @is_free_email_provider.setter
+    def is_free_email_provider(self, is_free_email_provider):
+        """Sets the is_free_email_provider of this FullEmailValidationResponse.
+
+        True if the email domain name is a free provider (typically a free to sign up web email provider for consumers / personal use), false otherwise.  # noqa: E501
+
+        :param is_free_email_provider: The is_free_email_provider of this FullEmailValidationResponse.  # noqa: E501
+        :type: bool
+        """
+
+        self._is_free_email_provider = is_free_email_provider
+
+    @property
+    def is_disposable(self):
+        """Gets the is_disposable of this FullEmailValidationResponse.  # noqa: E501
+
+        True if the email address is a disposable email address, false otherwise; these disposable providers are not typically used to receive email and so will have a low likelihood of opening mail sent there.  # noqa: E501
+
+        :return: The is_disposable of this FullEmailValidationResponse.  # noqa: E501
+        :rtype: bool
+        """
+        return self._is_disposable
+
+    @is_disposable.setter
+    def is_disposable(self, is_disposable):
+        """Sets the is_disposable of this FullEmailValidationResponse.
+
+        True if the email address is a disposable email address, false otherwise; these disposable providers are not typically used to receive email and so will have a low likelihood of opening mail sent there.  # noqa: E501
+
+        :param is_disposable: The is_disposable of this FullEmailValidationResponse.  # noqa: E501
+        :type: bool
+        """
+
+        self._is_disposable = is_disposable
 
     def to_dict(self):
         """Returns the model properties as a dict"""
